921886a2c9480dd88f328526af96d27cf2fa425d
Max Silbiger <max.silbiger@gmail.com>
2017-01-15 01:36:27 -0500

    Get n to display, but it still linebreaks afterward

--- n.py
+++ n.py
@@ -54,11 +54,44 @@ with open('original_ORFIELD.EXE', 'rb') as f:
 # 275 to 286: fine
 # 0 to 286: error message
 
-ns_to_replace = ns[100:101]
+# Messing with IDA now. The n's at 0x15b1d and 0x15b5f (109 and 110) changed to m's are promising:
+    # This seems to create a thing where it looks for "m*n" and uses that as a newline.
+    # But it blanks out anything between the m and the n, of course.
+    # So, why don't I set that first character to something never used (<) and use <n as the code?
+
+    # n displays now, but it still has the newlnie afterwards.
+
+# 109 and 110 replaced with <: displays n but still does a linebreak afterwards
+# 108, 109, 110: displays window, but can't display faces
+# 105, 109, 110: same as just 109 and 110?
+# 105, 106, 109, 110: Graphical glitches/crash. BUT it does display an n without a newline afterwards
+# 106, 109, 110: Worse graphical glitch than above, but still n without newline
+# 105, 106, 107, 109, 110: Milder graphical glitch than those two, but still n without newline
+# 105, 107, 109, 110: Doesn't display n's anymore, they're newlines again
+# 106, 107, 109, 110: Same as 105, 106, 109, 110
+# 104, 106, 109, 110: Like 105 106 109 110, but with more graphical glitches and crashes
+# 106, 109, 110, 111: Same, but more graphical glitches
+# 106, 109, 110, 112: Same
+# 106, 109, 110, 113: Same
+#                114: Same
+# 104, 106: Freaks out during the first line break, tons of graphical glitches
+
+
+# 104 15519 = cmp byte ptr es:[bx], 6e
+# 105 15528 = mov byte ptr [bx], 6e
+# 106 155d6 = cmp byte ptr es:[bx], 6e
+# 107 155ee = mov byte ptr [bx], 6e
+# 108 15624 = jmp loc_21D94 (yeah, so don't mess with this)
+# 109 15b1d = cmp byte ptr [bx+di], 6e
+# 110 15b5f = cmp byte ptr [bx+di], 6e
+
+# 6 to 69 (inclusive): that big table of n's
+
+ns_to_replace = ns[6:69] + [ns[109], ns[110]]
 print [hex(n) for n in ns_to_replace]
 
 for n in ns_to_replace:
-    file_contents = file_contents[:n] + "o" + file_contents[n+1:]
+    file_contents = file_contents[:n] + "<" + file_contents[n+1:]
 
 with open('ORFIELD.EXE', 'wb') as f:
     f.write(file_contents)
