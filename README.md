b74efa1faf3d82340580af2ad8645bf206cf99ac
hollowaytape <hollowaytape@retro-type.com>
2017-06-18 01:09:13 -0400

    Add remaining pointer constants, remove pointer locations below pointer constant

---
+++ README.md
@@ -0,0 +1,18 @@
+# appareden
+![Appareden Title](img/Appareden_title.png)
+
+Romhacking utilities and notes developed as part of [46 OkuMen](http://46okumen.com/)'s English translation patch for *Appareden - Fukuryuu no Shou* (あっぱれ伝 ー伏龍の章ー) (PC-98), a traditional and colorful JRPG based on Japanese mythology.
+
+### Reinsertion Progress
+| Segment      | %    |  Strings            | 
+| -------------|-----:|:-------------------:|
+| Title        |   0% |   (0 / 25)          |
+| Main         |   0% |   (0 / 204)         |
+| Field        |   0% |   (0 / 1064)        |
+| Battle       |   0% |   (0 / 709)         |
+| Cat Minigame |   0% |   (0 / 7)           |
+| Dialogue     |   0% |   (0 / 249 files)   |
+
+
+### How do I use this?
+* Wait for a release!
--- find_pointers.py
+++ find_pointers.py
@@ -17,7 +17,10 @@ Dump = DumpExcel(DUMP_XLS_PATH)
 
 
 OriginalAp = Disk(SRC_DISK, dump_excel=Dump)
-files_to_search = ['ORTITLE.EXE', 'ORMAIN.EXE']
+files_to_search = ['ORTITLE.EXE', 'ORMAIN.EXE', 'ORFIELD.EXE', 'ORBTL.EXE', 'SFIGHT.EXE']
+
+# NEKORUN.EXE might not have pointers. Edit it manually? Mostly error messages anyway.
+# ENDING.EXE is only error messages...?
 
 try:
     os.remove('appareden_pointer_dump.xlsx')
@@ -28,30 +31,48 @@ PtrXl = PointerExcel('appareden_pointer_dump.xlsx')
 for f in files_to_search:
     print(f)
     GF = Gamefile(os.path.join('original', f), disk=OriginalAp)
-    worksheet = PtrXl.add_worksheet(GF.filename)
+    try:
+        worksheet = PtrXl.add_worksheet(GF.filename)
+    except AttributeError:
+        _ = input("You have the sheet open, close it and hit Enter")
+        worksheet = PtrXl.add_worksheet(GF.filename)
     row = 1
     for block in FILE_BLOCKS[f]:
         blk = Block(GF, block)
-        last_pointer_location = 0
+        last_pointer_location = POINTER_CONSTANT[f]
+        pointer_location = last_pointer_location
         for t in Dump.get_translations(blk, include_blank=True):
             text_location = t.location
             look_for_int = t.location - POINTER_CONSTANT[f]
             look_for = look_for_int.to_bytes(2, byteorder='little')
-            if GF.filestring.count(look_for) == 1:
+            if GF.filestring.count(look_for) == 1 and GF.filestring.find(look_for) > POINTER_CONSTANT[f]:
                 pointer_location = GF.filestring.find(look_for)
+                print("%s: %s" % (hex(t.location), hex(pointer_location)))
             else:
-                pointer_location = "Not found"
+                all_locs = list(find_all(GF.filestring, look_for))
+                all_locs = [l for l in all_locs if l > POINTER_CONSTANT[f] ]
+                for a in all_locs:
+                    if last_pointer_location < a < last_pointer_location + 0x100:
+                        pointer_location = a
+                #pointer_location = "Not found"
 
-            print("%s: %s" % (hex(t.location), pointer_location))
+            if pointer_location == last_pointer_location:
+                pointer_location = "?"
+
             obj = BorlandPointer(GF, pointer_location, text_location)
             worksheet.write(row, 0, hex(text_location))
             try:
                 worksheet.write(row, 1, hex(pointer_location))
             except TypeError:
                 worksheet.write(row, 1, pointer_location)
+                worksheet.write(row, 3, "%s" % ([hex(a) for a in all_locs]))
+
+            worksheet.write(row, 2, obj.text())
             #try:
             #    worksheet.write(row, 2, obj.text())
             #except:
             #    worksheet.write(row, 2, '')
             row += 1
+            if pointer_location != "?":
+                last_pointer_location = pointer_location
 PtrXl.workbook.close()
