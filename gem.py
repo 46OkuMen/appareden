66d8ef421f075d059621574983a73730028244d1
hollowaytape <hollowaytape@retro-type.com>
2017-08-17 02:31:14 -0400

    Dumb encoder; works better on complicated geometry but might break ingame?

--- gem.py
+++ gem.py
@@ -7,8 +7,8 @@ from rominfo import DEST_DISK
 from PIL import Image
 from bitstring import BitArray
 
-d = Disk(DEST_DISK)
-d.insert('ORTITLE.GEM', path_in_disk='TGL/OR')
+#d = Disk(DEST_DISK)
+#d.insert('ORTITLE.GEM', path_in_disk='TGL/OR')
 
 
 NAMETAG_PALETTE = b'\x00\x03\x33\x38\x40\xf4\x4d\x94\xfb\xac\xb9\xfd\x80\x21\x57\xd0\x66\x87\x3a\xcf\x8b\xff\xff\xff\x00'
@@ -87,13 +87,18 @@ for b in range(blocks):
 
         pattern = BitArray(pattern).bytes
 
-        if pattern in unique_patterns:
-            pattern_locations[pattern].append(row_cursor)
-        elif pattern == b'\x00\x00\x00\x00':
-            pass
-        else:
-            unique_patterns.append(pattern)
-            pattern_locations[pattern] = [row_cursor,]
+
+        unique_patterns.append(pattern)
+
+
+        # Going to retire this "intelligent way of doing it" for now
+        #if pattern in unique_patterns:
+        #    pattern_locations[pattern].append(row_cursor)
+        #elif pattern == b'\x00\x00\x00\x00':
+        #    pass
+        #else:
+        #    unique_patterns.append(pattern)
+        #    pattern_locations[pattern] = [row_cursor,]
 
         row_cursor += 1
 
@@ -102,7 +107,7 @@ for p in pattern_locations:
     print(p)
 
 IMAGE_DATA_LOCATION = 0x29 + (len(unique_patterns)*4)    # where pattern data ends and image data begins.
-with open('ORTITLE.GEM', 'wb') as f:
+with open('BENIMARU.GEM', 'wb') as f:
     f.write(b'Gem')
     f.write(b'\x02\x04\x00\x0e\x00')
     f.write(b'\x18\x00') # not sure what these bytes do
@@ -114,36 +119,8 @@ with open('ORTITLE.GEM', 'wb') as f:
         f.write(p)
 
     row_cursor = 0
-    for pattern in unique_patterns:
-        for loc in pattern_locations[pattern]:
-            if pattern == unique_patterns[0] and loc == 0:
-                row_cursor += 1
-                continue
-            elif pattern == b'\x00\x00\x00\x00':
-                row_cursor += 1
-                continue
-
-            print(loc, row_cursor)
-
-            if loc == row_cursor:
-                f.write(b'\x41')
-            elif loc - row_cursor <= 63:
-                # To begin writing on row 2 (third row) and skip the first two rows, need to do 82.
-                # row_cursor = 0, loc = 2
-                # 
-                if row_cursor == 0:
-                    skip_and_write_code = 0x80 + (loc - row_cursor)
-                else:
-                    skip_and_write_code = 0x81 + (loc - row_cursor)
-                print(hex(skip_and_write_code))
-                f.write(skip_and_write_code.to_bytes(1, byteorder='little'))
-                row_cursor = loc
-            else:
-                raise Exception
-
-            row_cursor += 1
-        
-            print(pattern, loc)
+    for pattern in unique_patterns[1:]:
+        f.write(b'\x41')
         f.write(b'\x00')
 
 
@@ -158,4 +135,4 @@ with open('ORTITLE.GEM', 'wb') as f:
 
 
 d = Disk(DEST_DISK)
-d.insert('ORTITLE.GEM', path_in_disk='TGL/OR')
+d.insert('BENIMARU.GEM', path_in_disk='TGL/OR')
